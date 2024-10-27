d858e352b9e3e16c3b2c56f369a55cb19479843c
Henrique Sales <henriquedelgadosales@gmail.com>
2024-10-27 14:35:40 -0300

    Remove unused imports.

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,3 @@
-import CustomLink from "./custom-link";
-import packageJSON from "next-auth/package.json";
-
 export default function Footer() {
   return (
     <footer className="mx-0 my-4 flex w-full flex-col gap-4 px-4 text-sm sm:mx-auto sm:my-12 sm:h-5 sm:max-w-3xl sm:flex-row sm:items-center sm:justify-between sm:px-6">
